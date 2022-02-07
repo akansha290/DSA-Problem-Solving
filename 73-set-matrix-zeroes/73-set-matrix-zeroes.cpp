8d35de3b540e5cebfa78f861297c256b560e8bab
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-07 10:53:49 +0530

    Time: 32 ms (13.97%), Space: 13.3 MB (53.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        bool isZeroCol = false;
-        bool isZeroRow = false;
-        for (int i = 0; i < matrix.size(); i++) { //check the first column
-            if (matrix[i][0] == 0) {
-                isZeroCol = true;
-                break;
-            } 
-        }
-        for (int i = 0; i < matrix[0].size(); i++) { //check the first row
-            if (matrix[0][i] == 0) {
-                isZeroRow = true;
-                break;
-            } 
-        }
-        for (int i = 1; i < matrix.size(); i++) { //check except the first row and column
-            for (int j = 1; j < matrix[0].size(); j++) 
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+    void setZeroes(vector<vector<int>>& mat) {
+        int r = mat.size(), c=mat[0].size();
+        vector<int> row,col;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(mat[i][j]==0){
+                    row.push_back(i);
+                    col.push_back(j);
                 }
+            }
         }
-        for (int i = 1; i < matrix.size(); i++) { //process except the first row and column
-           for (int j = 1; j < matrix[0].size(); j++) 
-               if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                   matrix[i][j] = 0;
-        }
-        if (isZeroCol) { //handle the first column
-            for (int i = 0; i < matrix.size(); i++) 
-                matrix[i][0] = 0;
-        }
-        if (isZeroRow) { //handle the first row
-            for (int i = 0; i < matrix[0].size(); i++) 
-                matrix[0][i] = 0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(find(row.begin(),row.end(),i)!=row.end() or find(col.begin(),col.end(),j)!=col.end()){
+                    mat[i][j]=0;
+                }
+            }
         }
     }
 };
