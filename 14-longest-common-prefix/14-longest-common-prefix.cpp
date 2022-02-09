5614b90f3ef9272fc14ded1dd616ea3b0700f6d6
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-09 22:16:19 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (44.59%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& str) {
-        int n = str.size();
-        if(n==0) return "";
-        
-        string ans  = "";
-        sort(begin(str), end(str));
-        string a = str[0];
-        string b = str[n-1];
-        
-        for(int i=0; i<a.size(); i++){
-            if(a[i]==b[i]){
-                ans = ans + a[i];
-            }
-            else{
-                break;
-            }
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) {
+            return "";
         }
         
-        return ans;
+        // First let's assume that the entire first string
+        // is the common prefix.
+        //
+        std::string prefix = strs[0];
+        
+        // Now we iterate through the list of strings, and 
+        // for each string, we see how much of it matches with
+        // our prefix. The size of the prefix keeps decreasing.
+        //
+        for(int i = 1; i < strs.size(); ++i) {
+            // As long as you don't find the prefix, keep decreasing
+            // it's size.
+            //strs[i].find(prefix) returns index of 1st occuence of prefix in strs[i];
+            while(strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+            }
+            if(prefix.length() == 0) return "";
+        }
+        
+        return prefix;
     }
 };
