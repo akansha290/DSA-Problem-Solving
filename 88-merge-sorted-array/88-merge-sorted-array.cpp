8dbee5d968ec0520cddeeec58738db50d1407a13
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-12 11:21:10 +0530

    Time: 4 ms (53.69%), Space: 9.1 MB (72.65%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -5,20 +5,17 @@ public:
         int pointer = m+n-1;
         while(pointer>=0 and p1>=0 and p2>=0){
             if(nums1[p1]>=nums2[p2]){
-                nums1[pointer]=nums1[p1];
+                nums1[pointer] = nums1[p1];
                 p1--;
             }
-            else if(nums1[p1]<=nums2[p2]){
-                nums1[pointer]=nums2[p2];
+            else if(nums2[p2]>=nums1[p1]){
+                nums1[pointer] = nums2[p2];
                 p2--;
             }
             pointer--;
         }
         while(pointer>=0 and p2>=0){
-            nums1[pointer]=nums2[p2];
-            pointer--;
-            p2--;
+            nums1[pointer--] = nums2[p2--];
         }
-        return;
     }
 };
