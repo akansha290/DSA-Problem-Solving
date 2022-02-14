c423fc749bf19ef194f190be456a85007938780f
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-14 10:12:00 +0530

    Time: 8 ms (32.04%), Space: 9.7 MB (5.16%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
+    bool bs(vector<int>v,int target){
+        int lo=0,hi=v.size();
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(v[mid]==target) return true;
+            if(target<v[mid]) hi = mid-1;
+            else lo = mid+1;
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int m=mat.size(), n =mat[0].size();
+        int m=mat.size(),n=mat[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==target) return true;
-            }
+            vector<int> temp = mat[i];
+            if(mat[i][0]==target or mat[i][n-1]==target) return true;
+            else if(mat[i][0]<target and mat[i][n-1]>target)
+                return bs(temp,target);
         }
         return false;
     }
