e8cdea31ca15ff41681a19ca0d90cdbbbfe1a600
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-14 17:41:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.26%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    //DP Method
-    int dp(int m,int n,int i,int j, vector<vector<int>>& v){
-        if(i==m-1 and j==n-1) return 1;
-        if(i>=m or j>=n) return 0;
-        if(v[i][j]!=-1) return v[i][j];
-        return v[i][j] = dp(m,n,i+1,j,v) + dp(m,n,i,j+1,v); 
-    }
-    
-    //Gives TLE ->Recursion Method;
-    int rec(int m,int n,int i,int j){
-        if(i==m-1 and j==n-1) return 1;
-        if(i>=m or j>=n) return 0;
-        return rec(m,n,i+1,j) + rec(m,n,i,j+1);
-    }
     int uniquePaths(int m, int n) {
-        if(m==0 or n==0) return 0;
-        // return rec(m,n,0,0);
-        vector<vector<int>>v(m,vector<int>(n,-1));
-        return dp(m,n,0,0,v);
+        int totalSteps = m+n-2;
+        int r = min(m,n)-1;
+        double ans=1;
+        for(int i=1;i<=r;i++){
+            ans = ans *(totalSteps-r+i)/i;
+        }
+        return int(ans);
     }
 };
