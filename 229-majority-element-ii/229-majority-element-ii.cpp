8b782e42568fcd52492515ea984d8c8c99f45e5a
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-16 17:52:54 +0530

    Time: 3 ms (99.78%), Space: 15.8 MB (52.21%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //Boyer Moore Voting Algorithm
         int sz = nums.size();
-        int num1 = INT_MIN, num2 = INT_MIN;
-        int cnt1 = 0, cnt2 = 0;
-        for(auto num:nums){
-            if(num == num1) cnt1+=1;
-            else if(num == num2) cnt2+=1;
+        int can1 = INT_MIN, can2 = INT_MIN;
+        int cnt1=0,cnt2=0;
+        for(auto it:nums){
+            if(it==can1) cnt1++;
+            else if(it==can2) cnt2++;
             else if(cnt1==0){
-                num1 = num;
+                can1 = it;
                 cnt1 = 1;
             }
             else if(cnt2==0){
-                num2 = num;
+                can2 = it;
                 cnt2 = 1;
             }
             else{
@@ -24,14 +23,11 @@ public:
         vector<int> ans;
         cnt1=cnt2=0;
         for(auto it:nums){
-            if(it==num1) cnt1+=1;
-            if(it==num2) cnt2+=1;
+            if(it==can1) cnt1++;
+            else if(it==can2) cnt2++;
         }
-        // cout<<num1<<" "<<num2<<" "cnt1<<" "<<cnt2;
-        if(cnt1>(sz/3)) ans.push_back(num1);
-        if(cnt2>(sz/3)) ans.push_back(num2);
-        // if(cnt2>(sz/3) and num1!=num2) ans.push_back(num2);
-        
+        if(cnt1>(sz/3)) ans.push_back(can1);
+        if(cnt2>(sz/3)) ans.push_back(can2);
         return ans;
     }
 };
