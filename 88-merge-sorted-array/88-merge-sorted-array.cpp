5e1f8ebbb141b49fa616f5a91276abe3ce7caef8
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-16 09:34:49 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.72%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1=m-1,p2=n-1;
-        int pointer = m+n-1;
-        while(pointer>=0 and p1>=0 and p2>=0){
-            if(nums1[p1]>=nums2[p2]){
-                nums1[pointer] = nums1[p1];
-                p1--;
+        int i=m-1,j=n-1;
+        int total = m+n-1;
+        while(total>=0 and i>=0 and j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[total--]=nums1[i--];
             }
-            else if(nums2[p2]>=nums1[p1]){
-                nums1[pointer] = nums2[p2];
-                p2--;
+            else if(nums1[i]<nums2[j]){
+                nums1[total--]=nums2[j--];
             }
-            pointer--;
         }
-        while(pointer>=0 and p2>=0){
-            nums1[pointer--] = nums2[p2--];
+        while(total>=0 and i>=0){
+            nums1[total--]=nums1[i--];
+        }
+        while(total>=0 and j>=0){
+            nums1[total--]=nums2[j--];
         }
     }
 };
