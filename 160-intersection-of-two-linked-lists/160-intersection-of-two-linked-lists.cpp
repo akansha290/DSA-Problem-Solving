775664d55c51f2a379880937ce97c73e5ad2b095
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-17 09:45:51 +0530

    Time: 95 ms (20.17%), Space: 14.3 MB (99.15%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,18 +8,34 @@
  */
 class Solution {
 public:
+    int calc(ListNode *head){
+        int cnt=0;
+        while(head){
+            cnt++;
+            head = head->next;
+        }
+        return cnt;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==NULL or headB==NULL){
-            return NULL;
+        if(headA==NULL or headB==NULL) return NULL;
+        ListNode *ta=headA, *tb = headB;
+        int m = calc(ta);
+        int n = calc(tb);
+        ListNode *a = m>=n?headA:headB;
+        ListNode *b = m<n?headA:headB;
+        int cnt = 1;
+        int diff = abs(m-n);
+        // cout<<diff<<" "<<a->val<<" "<<b->val;
+        while(cnt<=diff){
+            a = a->next;
+            cnt+=1;
         }
-        ListNode *a = headA;
-        ListNode *b = headB;
-        while(a!=b){
-            
-            a = a==NULL?headB:a->next;
-            b = b==NULL?headA:b->next;
+        // cout<<"\n"<<a->val<<" "<<b->val;
+        while(a!=b and a and b){
+            a = a->next;
+            b = b->next;
         }
+        if(a==NULL or b==NULL) return NULL;
         return a;
-        
     }
 };
