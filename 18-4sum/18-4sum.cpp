87d731ecd2c61c342c704a2129991fe3aec7d930
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-17 19:02:54 +0530

    Time: 40 ms (89.49%), Space: 13.3 MB (51.47%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -4,28 +4,27 @@ public:
         vector<vector<int>> ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        if(n<4) return ans;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int target2 = target - nums[i] -nums[j];
+                int tar2 = target - nums[i] - nums[j];
                 int left=j+1, right=n-1;
                 while(left<right){
-                    
-                    int x = nums[left] + nums[right];
-                    if(x<target2) left++;
-                    else if(x>target2) right--;
+                    int x = nums[left]+nums[right];
+                    if(x<tar2) left++;
+                    else if(x>tar2) right--;
                     else{
-                        vector<int> quad(4,0);
-                        quad[0]=nums[i];
-                        quad[1]=nums[j];
-                        quad[2]=nums[left];
-                        quad[3]=nums[right];
-                    
-                    while(left<right and nums[left]==quad[2]) left++;
-                    while(left<right and nums[right]==quad[3]) right--;
-                    ans.push_back(quad);}
-                 }
-                
+                        vector<int> v(4,0);
+                        v[0] = nums[i];
+                        v[1] = nums[j];
+                        v[2] = nums[left];
+                        v[3] = nums[right];
+                        ans.push_back(v);
+                        
+                        while(left<right and nums[left]==v[2]) left++;
+                        while(left<right and nums[right]==v[3]) right--;
+                        
+                    }
+                }
                 while(j+1<n and nums[j+1]==nums[j]) j++;
             }
             while(i+1<n and nums[i+1]==nums[i]) i++;
