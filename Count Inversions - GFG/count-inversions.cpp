4f99a49994a4f216a7bb1116b2b477f204e66397
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-27 18:21:16 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -10,19 +10,19 @@ class Solution{
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
     long long merge(long long *arr,long long *temp,long long lo, long long mid,long long hi){
-        long long  i=lo,j=mid,k=lo;
+        long long  i=lo,j=mid+1,k=lo;
         long long  ans = 0;
         
-        while(i<=(mid-1) and j<=hi){
+        while(i<=(mid) and j<=hi){
             if(arr[i]<=arr[j]){
                 temp[k++] = arr[i++];
             }
             else{
-                ans += (mid-i);
+                ans = ans + (mid-i+1);
                 temp[k++] = arr[j++];
             }
         }
-        while(i<=mid-1){
+        while(i<=mid){
             temp[k++] = arr[i++];
         }
         while(j<=hi){
@@ -40,7 +40,7 @@ class Solution{
             mid = (lo+hi)/2;
             conv += mergesort(arr,temp,lo,mid);
             conv += mergesort(arr,temp,mid+1,hi);
-            conv += merge(arr,temp,lo,mid+1,hi);
+            conv += merge(arr,temp,lo,mid,hi);
         }
         return conv;
     }
