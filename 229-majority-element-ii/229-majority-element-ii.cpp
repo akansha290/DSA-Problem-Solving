dda2d14b2cf8735d8009bcba26d998d14602ae4d
Akansha Kumari <akanshakumari290@gmail.com>
2022-02-27 22:00:08 +0530

    Time: 16 ms (66.41%), Space: 15.8 MB (52.85%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int sz = nums.size();
+        int size = nums.size();
+        int cnt1 = 0, cnt2=0;
         int can1 = INT_MIN, can2 = INT_MIN;
-        int cnt1=0,cnt2=0;
-        for(auto it:nums){
-            if(it==can1) cnt1++;
-            else if(it==can2) cnt2++;
+        for(int i=0;i<size;i++){
+            if(nums[i]==can1){
+                cnt1+=1;
+            }
+            else if(nums[i]==can2){
+                cnt2+=1;
+            }
             else if(cnt1==0){
-                can1 = it;
-                cnt1 = 1;
+                can1 = nums[i];
+                cnt1=1;
             }
             else if(cnt2==0){
-                can2 = it;
-                cnt2 = 1;
+                can2 = nums[i];
+                cnt2=1;
             }
             else{
-                cnt1--;
-                cnt2--;
+                cnt1-=1;
+                cnt2-=1;
             }
         }
-        vector<int> ans;
-        cnt1=cnt2=0;
-        for(auto it:nums){
-            if(it==can1) cnt1++;
-            else if(it==can2) cnt2++;
+        // cout<<can1<<" "<<cnt1<<" "<<can2<<" "<<cnt2;
+        cnt1 = 0,cnt2=0;
+        for(int i=0;i<size;i++){
+            if(can1==nums[i]) cnt1+=1;
+            else if(can2==nums[i]) cnt2+=1;
         }
-        if(cnt1>(sz/3)) ans.push_back(can1);
-        if(cnt2>(sz/3)) ans.push_back(can2);
-        return ans;
+        vector<int> res;
+        if(cnt1>(size/3)) res.push_back(can1);
+        if(cnt2>(size/3)) res.push_back(can2);
+        return res;
     }
 };
