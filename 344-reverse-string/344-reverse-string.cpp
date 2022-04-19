1c9c1bcce1889683cb3ad5f0ca07f003b38adfce
Akansha Kumari <akanshakumari290@gmail.com>
2022-04-19 18:28:59 +0530

    Time: 22 ms (78.25%), Space: 23.3 MB (42.07%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    void rev(vector<char>& s,int lo,int hi){
+        if(lo>=(hi/2)) return;
+        // cout<<s[lo]<<" "<<s[hi-lo-1]<<endl;
+        swap(s[lo],s[hi-lo-1]);
+        rev(s,lo+1,hi);
+    }
     void reverseString(vector<char>& s) {
-        int lo=0,hi=s.size()-1;
-        while(lo<=hi){
-            swap(s[lo++],s[hi--]);
-        }
+        rev(s,0,s.size());
+        return;
     }
 };
