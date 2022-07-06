0c3aecd9d8495a56c4ba4d34e75e2ade00f6c3a0
Akansha Kumari <akanshakumari290@gmail.com>
2022-07-06 12:28:31 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (63.47%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,56 +11,24 @@
  */
 class Solution {
 public:
-    //2 Stack Approach
     vector<int> postorderTraversal(TreeNode* root) {
-    //     vector<int> ans;
-    //     if(!root) return ans;
-    //     stack<TreeNode *> st1,st2;
-    //     st1.push(root);
-    //     while(!st1.empty()){
-    //         root = st1.top();
-    //         st1.pop();
-    //         st2.push(root);
-    //         if(root->left) st1.push(root->left);
-    //         if(root->right) st1.push(root->right);
-    //     }
-    //     while(!st2.empty()){
-    //         ans.push_back(st2.top()->val);
-    //         st2.pop();
-    //     }
-    //     return ans;
-    
-    
-    //1 Stack Approach
-    vector<int> ans;
-    if(root==NULL) return ans;
-    
-    stack<TreeNode*> st;
-    
-    while(root!=NULL or !st.empty()){
-        if(root!=NULL){
-            st.push(root);
-            root=root->left;
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            
+            if(root->left) st1.push(root->left);
+            if(root->right) st1.push(root->right);
         }
-        else{
-            TreeNode *temp = st.top()->right;
-            if(temp==NULL){
-                temp = st.top();
-                st.pop();
-                ans.push_back(temp->val);
-                while(!st.empty() and temp==st.top()->right){
-                    temp = st.top();
-                    st.pop();
-                    ans.push_back(temp->val);
-                }
-                
-            }
-            else{
-                root = temp;
-            }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
         }
+        return ans;
     }
-    return ans;
-    }
-    
 };
