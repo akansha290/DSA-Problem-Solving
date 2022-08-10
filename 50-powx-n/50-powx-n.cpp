214e2305d9d8ec9a39ec759f56729d02ae79bd1c
Akansha Kumari <akanshakumari290@gmail.com>
2022-08-10 15:08:51 +0530

    Time: 4 ms (32.95%), Space: 6 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,10 +3,10 @@ public:
     double myPow(double x, int n) {
         double ans = 1.0;
         long long nn = n;
-        if(nn<0) nn = (-1)*nn;
-        while(nn){
-            if(nn%2){ 
-                ans= ans*x;
+        if(nn<0) nn = -1 * nn;
+        while(nn>0){
+            if(nn%2){
+                ans = ans*x;
                 nn-=1;
             }
             else{
@@ -14,7 +14,7 @@ public:
                 nn/=2;
             }
         }
-        if(n<0) ans = double(1.0)/ans;
+        if(n<0) ans = (double)(1.0)/(double) ans;
         return ans;
     }
 };
