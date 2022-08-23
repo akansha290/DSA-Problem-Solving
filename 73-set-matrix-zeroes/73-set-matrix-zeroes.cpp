35357910e64bd4de6e4713502464930966ff7102
Akansha Kumari <akanshakumari290@gmail.com>
2022-08-23 19:19:02 +0530

    Time: 25 ms (43.43%), Space: 13.2 MB (53.23%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -5,25 +5,25 @@ public:
         int col = matrix[0].size();
         bool firstcol = false;
         for(int i=0;i<row;i++){
-            if(matrix[i][0]==0)
+            if(matrix[i][0]==0){
                 firstcol = true;
+            }
             for(int j=1;j<col;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
-                
             }
         }
-        
         for(int i=row-1;i>=0;i--){
             for(int j=col-1;j>=1;j--){
                 if(matrix[i][0]==0 or matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
-            if(firstcol)
+            if(firstcol){
                 matrix[i][0]=0;
+            }
         }
     }
 };
